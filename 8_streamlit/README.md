6c44ef8a764ccbf0272743574d499b04653264b5
Kiana-Jahanshid <tannaz.maleki82@gmail.com>
2024-04-30 21:45:13 +0330

    2nd

--- 8_streamlit/README.md
+++ 8_streamlit/README.md
@@ -18,17 +18,56 @@ Then You can now view your Streamlit app in your browser :
 Local URL: http://localhost:8501
 
 
-## deployment :
+## deployment (counter App):
 You can see deploy button at the top of that page .
 
-+ in case of using docker :
-1_ create requirements.txt in file.py folder . containing streamlit , numpy , opencv-python <br>
-2_ create Dockerfile in this folder  ,use previous Dockerfile , but instead of uvicorn , write streamlit command :
-["streamlit" , "run" , "" , ""]  <br> 
-3_ build docker <br>
-4_ send to liara <br>
++ in case of using docker :<br>
+1_ create requirements.txt in counter.py folder . containing streamlit  <br>
+2_ create Dockerfile in this folder (counter_app) 
+
+```
+counter_app/
+├── counter.py
+├── Dockerfile
+└── requirements.txt
+```
++ Streamlit apps cannot be run from the root directory of Linux distributions. Your main script should live in a directory other than the root directory. so we push our files to github , then write github repository address in Dockerfile :
+
+```
+FROM python
+
+WORKDIR /8_streamlit/counter_app
+RUN git clone https://github.com/Kiana-Jahanshid/PyDeploy.git .
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+EXPOSE 8501
+HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
+COPY  . .
+ENTRYPOINT ["streamlit", "run", "counter.py", "--server.port=8501", "--server.address=0.0.0.0"]
+
+```
 
 
+3_ build a docker image : <br>
+```
+docker build -t streamlit .
+```
+
+4_ Run Docker container :
+```
+docker run -p 8501:8501 streamlit
+```
+
+5_ Deploy on Liara : <br>
+```
+npx liara login 
+npx liara deploy
+```
+
+# -----------------------------------------------------------------------------------------
+
+
+## deployment (data science App):
 + in case of selecting cloud :
 ```
 1_ push code on github <br>
---
+++ 8_streamlit/datascience_app/datascience.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# - [ ]  Upload a `.csv` file on the website
+# - [ ]  Convert csv data to `pandas dataframe`
+# - [ ]  Show dataframe in a table 
+# - [ ]  Draw a chart 
+# - [ ]  Write some information about your data science app in the website’s sidebar
+# - [ ]  Deploy it 🚀 using Streamlit Community Cloud
+
+
+st.set_page_config("Data Science App" , "📊")
+st.title("🎭🎬 Netflix Movies and TV Shows")
+st.subheader("Exploring the Depths of Netflix: A Comprehensive Dataset of Movies and TV Shows")
+
+st.write(" ")
+st.write(" ")
+
+df = pd.read_csv("netflix_titles.csv")
+df.to_csv().encode('utf-8')
+df = df[df.columns[:12]]
+df = df.dropna()
+df = df.drop(columns=["show_id"])
+st.dataframe(df )
+
+
+col1 , col2  = st.columns([6,6])
+with col1:
+    st.subheader('Distribution of Content Across Countries on Netflix')
+    country_counts = df['country'].value_counts().head(15).sort_values(ascending=False) # Displaying the top 15 countries
+    st.bar_chart(country_counts , width= 110 , color="#33FF57")
+    
+
+with col2:
+    st.subheader('Distribution of Content Types on Netflix')
+    content_type_counts = df['type'].value_counts()
+    #fig = plt.figure(figsize=(5, 4))
+    #content_type_counts.plot(kind='bar', color=['blue', 'red'] )
+    #st.pyplot(content_type_counts)
+    st.bar_chart(content_type_counts , width= 110 , color="#3CFEDA" )
+
+
+
+with st.sidebar:
+    st.subheader("About this App : ")
+    st.write("The Netflix Titles dataset is a comprehensive compilation of movies and TV shows available on Netflix, covering various aspects such as the title type, director, cast, country of production, release year, rating, duration, genres, and a brief description.")
+    st.write(" This dataset is instrumental for analyzing trends in Netflix content, understanding genre popularity, and examining the distribution of content across different regions and time periods.")
+
+
+    image = Image.open("netflix.jpg")
+
+st.sidebar.image(image)
+
+
+#st.sidebar()
